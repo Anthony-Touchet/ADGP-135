1062536e96044333c06df98225712831d65e6789
atouchetAIE <tocheck3@yahoo.com>
2016-01-25 17:43:59 -0600

    1/25/16
    Sprite implication done. Camera follows player.

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D playRB;
+    private Vector2 movement;
+    public int speed;
+
+    float maxSpeed = 10f;
+
+	void Lerp (float a, float b, float c) {//Lerp: moving by position
+        gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y + b, transform.position.z + c);
+	}
+	
+    void Start()
+    {
+        playRB = GetComponent<Rigidbody2D>();
+    }
+
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal"); //x Axis control
+        float v = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(h, v);
+
+        if (playRB.velocity.magnitude < maxSpeed)  //Keeps player at a speed
+        {
+            playRB.AddForce(movement * speed);
+        }
+
+    }
+}
