09fa90c36cb3f792a8b88a519b3711a5b7b7f6f3
atouchetAIE <tocheck3@yahoo.com>
2016-01-26 14:06:12 -0600

    1/26/16 AfterLunch Update
    Acomplished: Scene changing, collisions in order, sprites created:trees,
    doors, player; camera restraints.

---
+++ New Unity Project/Assets/Scripts/DoorWay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DoorWay : MonoBehaviour {
+
+    public string level; 
+        
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag.ToString() == "Player")
+        {
+            SceneManager.LoadScene(level);
+        }  
+    }      
+}
