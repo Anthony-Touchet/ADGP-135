3c5bbec895bbcb6563fe06c79f72193c9bf86006
atouchetAIE <tocheck3@yahoo.com>
2016-01-26 17:42:42 -0600

    1/26 Afternoon Update

---
+++ New Unity Project/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NPCMovement : MonoBehaviour {
+
+    public Transform[] patrolPoints;
+    public float speed;
+    public float moveRate;
+    public Text villagerText;
+
+    private int currentPoint;
+    private bool moving = true;
+    private float nextMove;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = patrolPoints[0].position;
+        currentPoint = 0;
+        villagerText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving == true)
+        {
+            if (transform.position == patrolPoints[currentPoint].position)
+             {
+                currentPoint++;
+                moving = false;
+                nextMove = Time.time + moveRate;
+             }
+
+              if (currentPoint >= patrolPoints.Length)
+             {
+                 currentPoint = 0;
+             }
+
+             transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, speed * Time.deltaTime);
+        }
+
+        if (Time.time > nextMove)
+        {
+            moving = true;
+            villagerText.text = "";
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            moving = false;
+            nextMove = Time.time + moveRate;
+            villagerText.text = "Hello";
+        }
+    }
+}
--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,6 @@ public class PlayerMovement : MonoBehaviour {
     private Vector2 movement;
     public int speed;
 
-    float maxSpeed = 10f;
-
 	void Lerp (float a, float b, float c) {//Lerp: moving by position
         gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y + b, transform.position.z + c);
 	}
@@ -22,12 +20,7 @@ public class PlayerMovement : MonoBehaviour {
         float h = Input.GetAxisRaw("Horizontal"); //x Axis control
         float v = Input.GetAxisRaw("Vertical");
 
-        movement = new Vector2(h, v);
-
-        if (playRB.velocity.magnitude < maxSpeed)  //Keeps player at a speed
-        {
-            playRB.AddForce(movement * speed);
-        }
-
+        movement = new Vector2(h, v);      
+        playRB.AddForce(movement * speed);
     }
 }
